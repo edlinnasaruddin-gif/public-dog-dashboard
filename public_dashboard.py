3227b823cd09268804acf672d0e5c783a3802930
edlinnasaruddin-gif <edlin.nasaruddin@gmail.com>
2026-01-17 17:18:10 +0800

    Update public_dashboard.py

--- public_dashboard.py
+++ public_dashboard.py
@@ -40,7 +40,7 @@ credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds, scope)
 gc = gspread.authorize(credentials)
 
 # Open the sheet by name or URL
-SHEET_URL = "https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID_HERE"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/d/1nDsxO0BV3hBCtn4FhtpXyRnKCGli6qW3oXSeewJZpbE/edit?gid=0#gid=0"
 sh = gc.open_by_url(SHEET_URL)
 worksheet = sh.sheet1  # or by name: sh.worksheet("Sheet1")
 
@@ -146,3 +146,4 @@ with st.expander("📄 Show dogs detected (count ≥ 1)"):
 # Footer
 # ----------------------------
 st.markdown("<hr><p style='text-align:center;color:gray;'>Powered by Streamlit & Google Sheets</p>", unsafe_allow_html=True)
+
